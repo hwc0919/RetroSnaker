d850320953b4672cf9da321b7255c39fd4f6a3bc
Wanchen He <hwc14@qq.com>
2019-12-20 22:33:31 +0800

    correct some spelling error

--- game_display.py
+++ game_display.py
@@ -6,7 +6,7 @@ from settings import *
 class GameDisplay():
     # draw the rectangular border of game area
     @staticmethod
-    def draw_boarder(screen):
+    def draw_board(screen):
         pygame.draw.line(screen, BORDER_COLOR, BOARD_LEFT_TOP,
                          BOARD_RIGHT_TOP, BORDER_WIDTH)
         pygame.draw.line(screen, BORDER_COLOR, BOARD_LEFT_BOTTOM,
@@ -41,7 +41,7 @@ class GameDisplay():
     # draw the whole game area
     @staticmethod
     def draw_game_area(screen, game_state, game_resource):
-        GameDisplay.draw_boarder(screen)
+        GameDisplay.draw_board(screen)
         if game_state.active:
             GameDisplay.draw_snake(screen, game_state.snake)
             if game_state.apple_eaten:
